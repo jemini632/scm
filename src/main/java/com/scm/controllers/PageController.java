d8af83e7529a384b880ae6c0aef60670108f19e6
jemini632 <jeminikr.1996@gmail.com>
2024-08-24 17:47:13 +0530

    layout

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,6 +1,9 @@
 package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -11,4 +14,20 @@ public class PageController {
           System.out.println("Home page handler");
           return "home";
        }
+
+       //about 
+
+       //services
+       @RequestMapping("/about")
+       public String about(){
+         System.out.println("About page");
+         return "about";
+       }
+
+       @RequestMapping("/services")
+       public String services() {
+         System.out.println("services paghe loading");
+           return "services";
+       }
+       
 }
