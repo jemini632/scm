a146e5e0396c2dcdbfe585e0eded3641298c02e6
jemini632 <jeminikr.1996@gmail.com>
2024-08-30 00:48:20 +0530

    processed form

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,8 +1,16 @@
 package com.scm.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+
+
+import com.scm.forms.UserForm;
 
 
 
@@ -40,9 +48,16 @@ public class PageController {
            return "login";
        }
 
+       
        @RequestMapping("/register")
        public String register() {
+        
            return "register";
        }
+
+      
        
-}
+ }
+       
+       
+
